035a8c4b89a27a1b1be5ce6bbd9cc07622d04120
Sokiryanin <eugene.sidorov@me.com>
2023-07-12 17:35:33 +0200

    fix gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,17 @@
+// ================================================================
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallerryRef = document.querySelector('.js-gallery');
+const divGallery = document.querySelector('.gallery');
+const gallaryMarkup = createGalleryCardMarkup(galleryItems);
 
-createMarkup(galleryItems, gallerryRef);
+divGallery.insertAdjacentHTML('beforeend', gallaryMarkup);
 
-function createMarkup(arr, list) {
-  const markup = arr
+function createGalleryCardMarkup(galleryItems) {
+  return galleryItems
     .map(
       ({ preview, original, description }) =>
         `<li class="gallety__item">
@@ -24,46 +26,9 @@ function createMarkup(arr, list) {
 </li>`
     )
     .join('');
-
-  list.innerHTML = markup;
 }
 
-const lightbox = new SimpleLightbox('.js-gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-
-// ================================================================
-// Add imports above this line
-// import { galleryItems } from "./gallery-items";
-
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-// const divGallery = document.querySelector(".gallery");
-// const gallaryMarkup = createGalleryCardMarkup(galleryItems);
-
-// divGallery.insertAdjacentHTML("beforeend", gallaryMarkup);
-
-// function createGalleryCardMarkup(galleryItems) {
-//   return galleryItems
-//     .map(
-//       ({ preview, original, description }) =>
-//         `<li class="gallety__item">
-// <a class="gallery__link"
-//   href="${original}">
-//   <img
-//   class="gallery__image"
-//   src="${preview}"
-//   data-source="${original}"
-//   alt="${description}">
-// </a>
-// </li>`
-//     )
-//     .join("");
-// }
-
-// const lightbox = new SimpleLightbox(".gallery a", {
-//   captionsData: "alt",
-//   captionDelay: 250,
-// });
